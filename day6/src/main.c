a57824d32957e03bb5d25b07c158beec1412c049
marius doerner <marius.dr1993@icloud.com>
2022-12-09 20:15:17 +0100

    day 6 done

---
+++ day6/src/main.c
@@ -0,0 +1,113 @@
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/ucontext.h>
+#include <errno.h>
+
+int PAGE_SIZE;
+
+extern int main(void);
+
+int syscall_write(char *msg, int64_t number, char base) {
+  write(1, msg, strlen(msg));
+  char buffer[sizeof(number) * 8];
+  char *p = &buffer[sizeof(number) * 8];
+  int len = -1; 
+  *(--p) = '\n';
+  if (0 < number) {
+    write(1, "-", 1);
+    number *= -1;
+  }
+  do {
+    *(--p) = "0123456789abcdef"[number % base];
+    number /= base;
+    len++;
+  } while (number != 0);
+  write(1, p, len);
+  return 0; 
+}
+
+volatile bool do_exit = false;
+
+static void sigsegv_action(int signo, siginfo_t *info, void *ctx) {
+  printf("SIGSEGV handler called at addr %p\n", info->si_addr);
+  void *addr = info->si_addr;
+  void *maddr = mmap(addr, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (maddr == MAP_FAILED) {
+    printf("mmap failed with errno %d\n", errno);
+  }
+  printf("mmap'd new page for %p starting at %p\n", addr, maddr);
+}
+
+static void install_sigsegv_handler(void) {
+  struct sigaction action;
+  action.sa_sigaction = sigsegv_action;
+  action.sa_flags = SA_SIGINFO;
+  sigaction(SIGSEGV, &action, NULL);
+}
+
+static void sigill_action(int signo, siginfo_t *info, void *ctx) {
+  ucontext_t *uctx =(ucontext_t *)ctx;
+  greg_t ip = uctx->uc_mcontext.gregs[REG_RIP];
+  printf("sigill at instruction ptr %llu ..\n", ip);
+  uctx->uc_mcontext.gregs[REG_RIP] += 4;
+  printf("incremented ip by 4 bytes \n");
+}
+
+static void install_sigill_handler(void) {
+  struct sigaction action;
+  action.sa_sigaction = sigill_action;
+  action.sa_flags = SA_SIGINFO;
+  sigaction(SIGILL, &action, NULL);
+}
+
+static void sigint_action(int signo, siginfo_t *info, void *ctx) {
+  printf("sigint occured, set do_exit\n");
+  do_exit = true;
+}
+
+static void install_sigint_handler(void) {
+  struct sigaction action;
+  action.sa_sigaction = sigint_action;
+  action.sa_flags = SA_SIGINFO;
+  sigaction(SIGINT, &action, NULL);
+}
+
+static void dump_pmap(void) {
+  char cmd[256];
+  snprintf(cmd, 256, "pmap %d", getpid());
+  printf("---- system(\"%s\"):\n", cmd);
+  system(cmd);
+}
+
+int main(void) {
+#define INVALID_OPCODE_32_BIT() __asm__("ud2; ud2;")
+  install_sigsegv_handler();
+  install_sigill_handler();
+  install_sigint_handler();
+
+  PAGE_SIZE = sysconf(_SC_PAGE_SIZE);
+  uint32_t *addr = (uint32_t *)0xdeadbeef;
+  *addr = 23;
+
+
+  INVALID_OPCODE_32_BIT();
+
+  while (!do_exit) {
+    sleep(1);
+    addr += 22559;
+    *addr = 42;
+    INVALID_OPCODE_32_BIT();
+  }
+
+  dump_pmap();
+  return EXIT_SUCCESS;
+}
